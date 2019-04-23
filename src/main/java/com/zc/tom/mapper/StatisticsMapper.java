9314d634b7e384916502c7bc2a9dd05f2217b784
YiWang <1124991320@qq.com>
2019-04-23 21:32:54 +0800

    归档

---
+++ src/main/java/com/zc/tom/mapper/StatisticsMapper.java
@@ -0,0 +1,10 @@
+package com.zc.tom.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface StatisticsMapper {
+     List<Map<String, Object>> statistics(@Param("classUUID") String classUUID);
+}
